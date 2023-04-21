f81dbdf67f675a7253f4e8e011e172b904b0a459
Derek Rowley <derekrowley@dotnetfx.com>
2023-04-20 22:51:41 -0600

    added shadcn components built on radix-ui

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-export { Card } from './components/card'
+export * from './components'
---
+++ src/stories/accordion.stories.js
@@ -0,0 +1,24 @@
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "../"
+
+const meta = {
+    title: "UI Blocks/Accordion",
+    component: Accordion,
+    tags: ["docsPage"],
+    argTypes: {
+        title: {
+            control: { type: "text" }
+        },
+        description: {
+            control: { type: "text" }
+        }
+    }
+}
+
+export default meta
+
+export const Primary = {
+    args: {
+        title: "Accordion Title",
+        description: "Lorum ipsum dolor sit amet."
+    }
+}
